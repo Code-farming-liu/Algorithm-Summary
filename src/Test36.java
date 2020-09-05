9ef15956ca9942d6f2e0865c915268644b387a65
liurui <2258076697@qq.com>
2020-09-05 19:53:31 +0800

    A number that appears only once in an array

---
+++ src/Test36.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @ClassName: Test36
+ * @Description: 题目描述
+ * 一个整型数组里除了两个数字之外，其他的数字都出现了两次。
+ * 请写程序找出这两个只出现一次的数字。
+ * @Author: Admin
+ **/
+
+public class Test36 {
+    public void FindNumsAppearOnce(int[] array, int num1[], int num2[]) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < array.length; i++) {
+            if (map.containsKey(array[i])) {
+                map.put(array[i], 2);
+            } else {
+                map.put(array[i], 1);
+            }
+        }
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (map.get(array[i]) == 1) {
+                if (count == 0) {
+                    num1[0] = array[i];
+                    count++;
+                } else {
+                    num2[0] = array[i];
+                }
+            }
+        }
+    }
+}

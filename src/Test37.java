4a642209d43bf8b79dc4ea79e8e1f1683596e6a1
liurui <2258076697@qq.com>
2020-09-06 21:00:48 +0800

    Sequence of continuous positive numbers with sum s

--- src/Test37.java
+++ src/Test37.java
@@ -13,31 +13,35 @@ import java.util.ArrayList;
  * 输出描述:
  * 输出所有和为S的连续正数序列。序列内按照从小至大的顺序，序列间按照开始数字从小到大的顺序
  * @Author: Admin
+ * @Date 2020/9/6 18:47
  **/
 
 public class Test37 {
-    public ArrayList<ArrayList<Integer>> FindContinuousSequence(int sum) {
-        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
-        if(sum <= 2){
-            return res;
-        }
-        int slow = 1;
-        int fast = 2;
-        while(fast > slow) {
-            int cur = (fast + slow) * (fast - slow + 1) / 2;
-            if(cur == sum) {
+    public ArrayList<ArrayList<Integer> > FindContinuousSequence(int sum) {
+        //存放结果
+        ArrayList<ArrayList<Integer> > result = new ArrayList<>();
+        //两个起点，相当于动态窗口的两边，根据其窗口内的值的和来确定窗口的位置和大小
+        int plow = 1,phigh = 2;
+        while(phigh > plow){
+            //由于是连续的，差为1的一个序列，那么求和公式是(a0+an)*n/2
+            // 等差数列 求和 (a1 + an) * n / 2  n = (fast - slow + 1)
+            int cur = (phigh + plow) * (phigh - plow + 1) / 2;
+            //相等，那么就将窗口范围的所有数添加进结果集
+            if(cur == sum){
                 ArrayList<Integer> list = new ArrayList<>();
-                for(int i = slow; i <= fast; i++){
+                for(int i=plow;i<=phigh;i++){
                     list.add(i);
                 }
-                res.add(list);
-                fast++;
-            } else if (cur < sum){
-                fast++;
-            } else {
-                slow++;
+                result.add(list);
+                plow++;
+                //如果当前窗口内的值之和小于sum，那么右边窗口右移一下
+            }else if(cur < sum){
+                phigh++;
+            }else{
+                //如果当前窗口内的值之和大于sum，那么左边窗口右移一下
+                plow++;
             }
         }
-        return res;
+        return result;
     }
 }

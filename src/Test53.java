99fa35524e18ef375d1e35df800b96c8af2c5729
liurui <2258076697@qq.com>
2020-10-21 19:33:14 +0800

    Sum of three numbers

---
+++ src/Test53.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @ClassName: Test53
+ * @Description: 三数之和
+ * 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。
+ * <p>
+ * 注意：答案中不可以包含重复的三元组。
+ * <p>
+ * <p>
+ * <p>
+ * 示例：
+ * <p>
+ * 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+ * <p>
+ * 满足要求的三元组集合为：
+ * [
+ * [-1, 0, 1],
+ * [-1, -1, 2]
+ * ]
+ * @Author: Admin
+ **/
+
+public class Test53 {
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList();
+        int len = nums.length;
+        if (nums == null || len < 3) {
+            return ans;
+        }
+        Arrays.sort(nums); // 排序
+        for (int i = 0; i < len; i++) {
+            if (nums[i] >= 0) {
+                break; // 如果当前数字大于0，则三数之和一定大于0，所以结束循环
+            }
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue; // 去重
+            }
+            int L = i + 1;
+            int R = len - 1;
+            while (L < R) {
+                int sum = nums[i] + nums[L] + nums[R];
+                if (sum == 0) {
+                    ans.add(Arrays.asList(nums[i], nums[L], nums[R]));
+                    while (L < R && nums[L] == nums[L + 1]) {
+                        L++; // 去重
+                    }
+                    while (L < R && nums[R] == nums[R - 1]) {
+                        R--; // 去重
+                    }
+                    L++;
+                    R--;
+                } else if (sum < 0) {
+                    L++;
+                } else if (sum > 0) {
+                    R--;
+                }
+            }
+        }
+        return ans;
+    }
+}

c48e3d9a051cae51dcbb70861e63b6acdd398d39
liurui <2258076697@qq.com>
2020-11-20 11:25:26 +0800

    Looking for peaks

---
+++ src/Test85.java
@@ -0,0 +1,54 @@
+/**
+ * @ClassName: Test85
+ * Description: 寻找峰值
+ * 峰值元素是指其值大于左右相邻值的元素。
+ * <p>
+ * 给定一个输入数组 nums，其中 nums[i] ≠ nums[i+1]，找到峰值元素并返回其索引。
+ * <p>
+ * 数组可能包含多个峰值，在这种情况下，返回任何一个峰值所在位置即可。
+ * <p>
+ * 你可以假设 nums[-1] = nums[n] = -∞。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: nums = [1,2,3,1]
+ * 输出: 2
+ * 解释: 3 是峰值元素，你的函数应该返回其索引 2。
+ * 示例 2:
+ * <p>
+ * 输入: nums = [1,2,1,3,5,6,4]
+ * 输出: 1 或 5
+ * 解释: 你的函数可以返回索引 1，其峰值元素为 2；
+ *      或者返回索引 5， 其峰值元素为 6。
+ * <p>
+ * @Author: Admin
+ **/
+
+public class Test85 {
+    //    一次遍历 只用判断前一个是否大于后一个数
+//     如果大于下一个数
+//     证明 肯定是下降趋势 因此直接返回对应的这个端点  这个端点是峰值
+//    如果小于这个数
+//     证明 上升趋势 然后继续往后寻找
+//     找到最后一个上升的数 这个数就是峰值
+    public int findPeakElement1(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                return i;
+            }
+        }
+        return nums.length - 1;
+    }
+    public int findPeakElement(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}

141b5422ecc8e189dc7536889af97f11889d7ef4
liurui <2258076697@qq.com>
2020-11-23 11:15:50 +0800

    The kth largest element in the array

---
+++ src/Test92.java
@@ -0,0 +1,184 @@
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Random;
+
+/**
+ * @ClassName: Test92
+ * Description: 数组中的第k个最大的元素
+ * 在未排序的数组中找到第 k 个最大的元素。请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [3,2,1,5,6,4] 和 k = 2
+ * 输出: 5
+ * 示例 2:
+ * <p>
+ * 输入: [3,2,3,1,2,4,5,5,6] 和 k = 4
+ * 输出: 4
+ * 说明:
+ * <p>
+ * 你可以假设 k 总是有效的，且 1 ≤ k ≤ 数组的长度。
+ * <p>
+ * @Author: Admin
+ **/
+
+public class Test92 {
+    //暴力破解 先排序 后判断
+    public int findKthLargest1(int[] nums, int k) {
+        Arrays.sort(nums);
+        return nums[nums.length - k];
+    }
+
+    //最小堆
+    public int findKthLargest2(int[] nums, int k) {
+        // init heap 'the smallest element first'
+        PriorityQueue<Integer> heap =
+                new PriorityQueue<>((n1, n2) -> n1 - n2);
+
+        // keep k largest elements in the heap
+        for (int n : nums) {
+            heap.add(n);
+            if (heap.size() > k) {
+                heap.poll();
+            }
+        }
+
+        // output
+        return heap.poll();
+    }
+
+    int[] nums;
+
+    public void swap(int a, int b) {
+        int tmp = this.nums[a];
+        this.nums[a] = this.nums[b];
+        this.nums[b] = tmp;
+    }
+
+
+    public int partition(int left, int right, int pivot_index) {
+        int pivot = this.nums[pivot_index];
+        // 1. move pivot to end
+        swap(pivot_index, right);
+        int store_index = left;
+
+        // 2. move all smaller elements to the left
+        for (int i = left; i <= right; i++) {
+            if (this.nums[i] < pivot) {
+                swap(store_index, i);
+                store_index++;
+            }
+        }
+
+        // 3. move pivot to its final place
+        swap(store_index, right);
+
+        return store_index;
+    }
+
+    //
+    public int quickselect(int left, int right, int k_smallest) {
+    /*
+    Returns the k-th smallest element of list within left..right.
+    */
+
+        if (left == right) { // If the list contains only one element,
+            return this.nums[left];  // return that element
+        }
+
+        // select a random pivot_index
+        Random random_num = new Random();
+        int pivot_index = left + random_num.nextInt(right - left);
+
+        pivot_index = partition(left, right, pivot_index);
+
+        // the pivot is on (N - k)th smallest position
+        if (k_smallest == pivot_index) {
+            return this.nums[k_smallest];
+        }
+        // go left side
+        else if (k_smallest < pivot_index) {
+            return quickselect(left, pivot_index - 1, k_smallest);
+        }
+        // go right side
+        return quickselect(pivot_index + 1, right, k_smallest);
+    }
+
+    public int findKthLargest4(int[] nums, int k) {
+        this.nums = nums;
+        int size = nums.length;
+        // kth largest is (N - k)th smallest
+        return quickselect(0, size - 1, size - k);
+    }
+
+    public int findKthLargest3(int[] nums, int k) {
+        k = nums.length - k;//注意这里的k已经变了
+        int lo = 0, hi = nums.length - 1;
+        while (lo <= hi) {
+            int i = lo;
+            //这里把数组以A[lo]的大小分为两部分，
+            //一部分是小于A[lo]的，一部分是大于A[lo]的
+            // [lo,i]<A[lo]，[i+1,j)>=A[lo]
+            for (int j = lo + 1; j <= hi; j++) {
+                if (nums[j] < nums[lo]) {
+                    swap(nums, j, ++i);
+                }
+            }
+            swap(nums, lo, i);
+            if (k == i) {
+                return nums[i];
+            } else if (k < i) {
+                hi = i - 1;
+            } else {
+                lo = i + 1;
+            }
+        }
+        return -1;
+    }
+
+    private void swap1(int[] nums, int i, int j) {
+        if (i != j) {
+            nums[i] ^= nums[j];
+            nums[j] ^= nums[i];
+            nums[i] ^= nums[j];
+        }
+    }
+
+    Random random = new Random();
+
+    public int findKthLargest(int[] nums, int k) {
+        return quickSelect(nums, 0, nums.length - 1, nums.length - k);
+    }
+
+    public int quickSelect(int[] a, int l, int r, int index) {
+        int q = randomPartition(a, l, r);
+        if (q == index) {
+            return a[q];
+        } else {
+            return q < index ? quickSelect(a, q + 1, r, index) : quickSelect(a, l, q - 1, index);
+        }
+    }
+
+    public int randomPartition(int[] a, int l, int r) {
+        int i = random.nextInt(r - l + 1) + l;
+        swap(a, i, r);
+        return partition(a, l, r);
+    }
+
+    public int partition(int[] a, int l, int r) {
+        int x = a[r], i = l - 1;
+        for (int j = l; j < r; ++j) {
+            if (a[j] <= x) {
+                swap(a, ++i, j);
+            }
+        }
+        swap(a, i + 1, r);
+        return i + 1;
+    }
+
+    public void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+}

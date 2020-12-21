dcb176b4e778744e58acf589839d47b4985c393c
liurui <2258076697@qq.com>
2020-12-21 17:18:05 +0800

    二叉树的镜像

--- src/offer/Code_18.java
+++ src/offer/Code_18.java
@@ -1,8 +1,10 @@
+package offer;
+
 import java.util.Stack;
 
 /**
  * @ClassName: Test18
- * @Description: 题目描述
+ * @Description: 二叉树的镜像
  * 操作给定的二叉树，将其变换为源二叉树的镜像。
  * 输入描述:
  * 二叉树的镜像定义：源二叉树
@@ -20,7 +22,38 @@ import java.util.Stack;
  * @Author: Admin
  **/
 
-public class Test18 {
+public class Code_18 {
+    /**
+     * @Author: Admin
+     * @Description: 思路描述：
+     * 我们根据图中的结果可以看出其特点
+     *
+     * 左子树的左节点 与 右子树的右节点 进行交换
+     *
+     * 左子树的 右节点 与 右子树的左节点 进行交换
+     *
+     * 首先我们将对应左右节点压入栈中，然后对应的进行交换，这样解释起来较为抽象 下面利用图解方式来进行。
+     *
+     * 	    8
+     * 	   /  \
+     * 	  6   10 经过调换左右子树之后
+     * 	 / \  / \
+     * 	5  7 9 11
+     *
+     * 	    8
+     * 	   /  \
+     * 	 10    6 调换左右子树的节点
+     * 	 / \   / \
+     * 	9 11   5  7
+     *
+     * 	    8
+     * 	   /  \
+     * 	  10   6
+     * 	 / \  / \
+     * 	11 9 7  5
+     * @param root
+     * @return: void
+     */
     public void Mirror(TreeNode root) {
         if(root == null) {
             return ;

f80ff31e4ad0eb5633a8202f384d3ce5fba9a9c9
liurui <2258076697@qq.com>
2020-12-21 20:32:13 +0800

    孩子们的游戏（圆圈中最后剩下的数）

--- src/Code_46.java
+++ src/Code_46.java
@@ -1,6 +1,6 @@
 /**
  * @ClassName: Test41
- * @Description: 题目描述
+ * @Description: 孩子们的游戏（圆圈中最后剩下的数）
  * 每年六一儿童节,牛客都会准备一些小礼物去看望孤儿院的小朋友,
  * 今年亦是如此。HF作为牛客的资深元老,自然也准备了一些小游戏。
  * 其中,有个游戏是这样的:首先,让小朋友们围成一个大圈。然后,
@@ -10,18 +10,25 @@
  * 这样下去....直到剩下最后一个小朋友,可以不用表演,并且拿到牛客名贵的
  * “名侦探柯南”典藏版(名额有限哦!!^_^)。请你试着想下,哪个小朋友会得到这份礼品呢？
  * (注：小朋友的编号是从0到n-1)
- *
+ * <p>
  * 如果没有小朋友，请返回-1
  * @Author: Admin
  **/
 
-public class Test41 {
+public class Code_46 {
+    /**
+     * @param n
+     * @param m
+     * @Author: Admin
+     * @Description: 我们直接模拟一个环形链表即可。
+     * @return: int
+     */
     public int LastRemaining_Solution(int n, int m) {
-        if(n == 0 || m == 0) {
+        if (n == 0 || m == 0) {
             return -1;
         }
         int cur = 0;
-        for(int i = 2; i <= n; i++) {
+        for (int i = 2; i <= n; i++) {
             cur = (cur + m) % i;
         }
         return cur;

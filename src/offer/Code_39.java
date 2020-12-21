386c84cd26e6562655bc22df709e962f325b1851
liurui <2258076697@qq.com>
2020-12-21 20:03:19 +0800

    平衡二叉树

--- src/offer/Code_39.java
+++ src/offer/Code_39.java
@@ -1,13 +1,25 @@
+package offer;
+
 /**
  * @ClassName: Test35
- * @Description: 题目描述
+ * @Description: 平衡二叉树
  * 输入一棵二叉树，判断该二叉树是否是平衡二叉树。
  * <p>
  * 在这里，我们只需要考虑其平衡性，不需要考虑其是不是排序二叉树
  * @Author: Admin
  **/
 
-public class Test35 {
+public class Code_39 {
+    /**
+     * @param root
+     * @Author: Admin
+     * @Description: 思路描述：
+     * 在做这道题之前，我们首先先来了解了解什么是平衡二叉树，它有什么特点
+     * 它的特点就是左右子树的高度差不能大于1，至于树的深度我们已经求解过了，
+     * 因此我们只需要求出左右子树的深度之后，将其做差查看结果是否小于1
+     * 如果 小于1 证明为二叉平衡树，否则不是。
+     * @return: boolean
+     */
     public boolean IsBalanced_Solution(TreeNode root) {
         return help(root) != -1;
     }

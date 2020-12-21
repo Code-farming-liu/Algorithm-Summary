de445a61e71d058ee4aa6302ffc3e573ea655347
liurui <2258076697@qq.com>
2020-12-21 17:14:29 +0800

    树的子结构

--- src/offer/Code_17.java
+++ src/offer/Code_17.java
@@ -1,12 +1,33 @@
+package offer;
+
 /**
  * @ClassName: Test17
- * @Description: 题目描述
+ * @Description: 树的子结构
  * 输入两棵二叉树A，B，判断B是不是A的子结构。（ps：我们约定空树不是任意一个树的子结构）
  * @Author: Admin
  * @Date 2020/9/3 11:43
  **/
 
-public class Test17 {
+public class Code_17 {
+    /**
+     * @param root1
+     * @param root2
+     * @Author: Admin
+     * @Description: 思路描述
+     * 我们首先分为几种情况考虑：
+     * 1.根节点相同 root1.val == root2.val ，
+     *  1.1 判断对应的子树节点的值是否相同
+     *  1.2 如果root2遍历完成，证明匹配成功 返回 true
+     *  1.3 如果root1为null，并且root2不为null 证明匹配失败，返回false
+     *  1.4 不断递归遍历比较，
+     * root1的左子树与root2的左子树，
+     * root1的右子树与root2的右子树
+     * 2. 根节点不同，那么我们就没办法，只能分别去比较
+     *  root1的左子树中是否包含root2;
+     *  root1的右子树中是否包含root2
+     *  包含则返回true，否则false
+     * @return: boolean
+     */
     public boolean HasSubtree(TreeNode root1, TreeNode root2) {
         //建立标志
         boolean res = false;
@@ -25,7 +46,6 @@ public class Test17 {
 
         }
         return res;
-
     }
 
     //当根节点相同 判断对应的左右子树 是否相同 匹配

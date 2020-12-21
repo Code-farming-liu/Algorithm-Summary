1be713d1a080c5f5636e26d55372d36bab10031e
liurui <2258076697@qq.com>
2020-12-21 20:22:14 +0800

    和为S的两个数字

--- src/offer/Code_42.java
+++ src/offer/Code_42.java
@@ -1,8 +1,10 @@
+package offer;
+
 import java.util.ArrayList;
 
 /**
  * @ClassName: Test38
- * @Description: 题目描述
+ * @Description: 和为S的两个数字
  * 输入一个递增排序的数组和一个数字S，在数组中查找两个数，
  * 使得他们的和正好是S，如果有多对数字的和等于S，输出两个数的乘积最小的。
  * 输出描述:
@@ -10,21 +12,33 @@ import java.util.ArrayList;
  * @Author: Admin
  **/
 
-public class Test38 {
+public class Code_42 {
+    /**
+     * @param array
+     * @param sum
+     * @Author: Admin
+     * @Description: 思路描述：
+     * 我们可以提取到是一个递增排序的数组，因此我们可以
+     * 使用对应的双指针，左指针 left 指向数组开始位置，右指针指向数组结束的位置 ，
+     * 如果当前值小于目标值，则left++；
+     * 如果当前值大于目标值，则right--；
+     * 否则将该值加入到集合当中并返回。
+     * @return: java.util.ArrayList<java.lang.Integer>
+     */
     public ArrayList<Integer> FindNumbersWithSum(int[] array, int sum) {
         ArrayList<Integer> res = new ArrayList<>();
-        if(sum <= 2) {
+        if (sum <= 2) {
             return res;
         }
         int slow = 0;
         int fast = array.length - 1;
-        while(slow < fast) {
+        while (slow < fast) {
             int cur = array[slow] + array[fast];
-            if(cur == sum) {
+            if (cur == sum) {
                 res.add(array[slow]);
                 res.add(array[fast]);
                 break;
-            } else if(cur < sum){
+            } else if (cur < sum) {
                 slow++;
             } else {
                 fast--;

76e8053b1e876dd915ba183f9ec8eafa2d2dec45
liurui <2258076697@qq.com>
2020-12-21 17:05:52 +0800

    链表中倒数第K个节点

--- src/offer/Code_14.java
+++ src/offer/Code_14.java
@@ -1,11 +1,25 @@
+package offer;
+
+import offer.ListNode;
+
 /**
  * @ClassName: Test14
- * @Description: 题目描述
+ * @Description: 链表中倒数第K个节点
  * 输入一个链表，输出该链表中倒数第k个结点。
  * @Author: Admin
  **/
 
-public class Test14 {
+public class Code_14 {
+    /**
+     * @Author: Admin
+     * @Description:思路描述：
+     *
+     * 我们先确定链表的长度
+     * 然后利用长度 - k 去找到要删除节点的前一个节点将其删除即可
+     * @param head
+     * @param k
+     * @return: offer.ListNode
+     */
     public ListNode FindKthToTail(ListNode head, int k) {
         if (head == null) {
             return null;

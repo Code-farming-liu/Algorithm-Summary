bc59bc2fe508deaf4f62030aefb853aea47378ba
liurui <2258076697@qq.com>
2020-12-22 16:05:44 +0800

    链表中环的入口节点

--- src/offer/Code_52.java
+++ src/offer/Code_52.java
@@ -1,13 +1,18 @@
+package offer;
+
+import offer.ListNode;
+
 import java.util.HashSet;
 import java.util.Set;
 
 /**
  * @ClassName: Test46
- * @Description: 给一个链表，若其中包含环，请找出该链表的环的入口结点，否则，输出null。
+ * @Description: 链表中环的入口节点
+ * 给一个链表，若其中包含环，请找出该链表的环的入口结点，否则，输出null。
  * @Author: Admin
  **/
 
-public class Test46 {
+public class Code_52 {
     public ListNode EntryNodeOfLoop(ListNode pHead) {
         if (pHead == null) {
             return pHead;

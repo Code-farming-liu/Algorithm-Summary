6d87c3e8fe8854c2a24429e2576627bf2a6b4359
liurui <2258076697@qq.com>
2020-12-22 15:59:58 +0800

    把字符串转换成整数

--- src/offer/Code_49.java
+++ src/offer/Code_49.java
@@ -1,6 +1,8 @@
+package offer;
+
 /**
  * @ClassName: Test43
- * @Description: 题目描述
+ * @Description: 把字符串转换成整数
  * 将一个字符串转换成一个整数，要求不能使用字符串转换整数的库函数。 数值为0或者字符串不是一个合法的数值则返回0
  * 输入描述:
  * 输入一个字符串,包括数字字母符号,可以为空
@@ -18,29 +20,38 @@
  * @Author: Admin
  **/
 
-public class Test43 {
+public class Code_49 {
+    /**
+     * @param str
+     * @Author: Admin
+     * @Description: 思路描述：
+     *
+     * 我们可以简单的建对应的字符串 转换成char数组，之后根据符号还是数字去判断当前应该是什么，
+     * 判断是否越界 即可
+     * @return: int
+     */
     public int StrToInt(String str) {
-        if(str == null || str.length() == 0) {
+        if (str == null || str.length() == 0) {
             return 0;
         }
         long res = 0;
         boolean is = str.charAt(0) == '-';
-        for(int i = 0; i < str.length();i++) {
+        for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
-            if(i == 0 && (c == '-' || c == '+')) {
+            if (i == 0 && (c == '-' || c == '+')) {
                 continue;
             }
-            if(c < '0' || c > '9') {
+            if (c < '0' || c > '9') {
                 return 0;
             }
             res = res * 10 + (c - '0');
         }
-        if(is == true && (-res) < Integer.MIN_VALUE) {
+        if (is == true && (-res) < Integer.MIN_VALUE) {
             return 0;
         }
-        if(is == true && res > Integer.MAX_VALUE) {
+        if (is == true && res > Integer.MAX_VALUE) {
             return 0;
         }
-        return is ? (int)-res : (int)res;
+        return is ? (int) -res : (int) res;
     }
 }

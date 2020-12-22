eec303af63a05835c4e1bf6f782bb544f5e6f418
liurui <2258076697@qq.com>
2020-12-22 17:39:38 +0800

    在排序数组中查找元素的第一个和最后一个元素的位置

--- src/LeetCode/Code_34.java
+++ src/LeetCode/Code_34.java
@@ -1,25 +1,34 @@
+package LeetCode;
+
 /**
  * @ClassName: Test58
  * @Description: 在排序数组中查找元素的第一个和最后一个元素的位置
- *  给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
- *
+ * 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+ * <p>
  * 你的算法时间复杂度必须是 O(log n) 级别。
- *
+ * <p>
  * 如果数组中不存在目标值，返回 [-1, -1]。
- *
+ * <p>
  * 示例 1:
- *
+ * <p>
  * 输入: nums = [5,7,7,8,8,10], target = 8
  * 输出: [3,4]
  * 示例 2:
- *
+ * <p>
  * 输入: nums = [5,7,7,8,8,10], target = 6
  * 输出: [-1,-1]
- *
  * @Author: Admin
  **/
 
-public class Test58 {
+public class Code_34 {
+    /**
+     * @param nums
+     * @param target
+     * @Author: Admin
+     * @Description: 思路描述：
+     * 暴力破解 较为简单 即第一个值保存刚开始的下标，第二个值一直保存到最后出现的那个数字的下标
+     * @return: int[]
+     */
     public static int[] searchRange1(int[] nums, int target) {
         int[] res = {-1, -1};
         if (nums.length == 0) {
@@ -54,6 +63,19 @@ public class Test58 {
     }
 
     //二分查找法
+
+    /**
+     * @param nums
+     * @param target
+     * @Author: Admin
+     * @Description: 思路描述：
+     * 因为题目中给出了是 有序数组并且时间复杂度是 O(logn)
+     * 因此很容就想到 肯定需要使用到二分查找法
+     * 我们直接利用二分法查找开始的元素下标 为mid
+     * 二分法查找最后一个元素的下表 mid + 1
+     * 因为是mid + 1 因此返回 left - 1
+     * @return: int[]
+     */
     public static int[] searchRange(int[] nums, int target) {
         int[] result = new int[2];
         result[0] = searchLeft(nums, target);

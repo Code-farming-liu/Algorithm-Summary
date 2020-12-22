c4d03cbbb2ff4c8af14e23f3e6f3ca27192dd72f
liurui <2258076697@qq.com>
2020-12-22 17:17:05 +0800

    三数之和

--- src/LeetCode/Code_15.java
+++ src/LeetCode/Code_15.java
@@ -1,3 +1,5 @@
+package LeetCode;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -5,7 +7,8 @@ import java.util.List;
 /**
  * @ClassName: Test53
  * @Description: 三数之和
- * 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。
+ * 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？
+ * 请你找出所有满足条件且不重复的三元组。
  * <p>
  * 注意：答案中不可以包含重复的三元组。
  * <p>
@@ -23,7 +26,21 @@ import java.util.List;
  * @Author: Admin
  **/
 
-public class Test53 {
+public class Code_15 {
+
+    /**
+     * @param nums
+     * @Author: Admin
+     * @Description: 思路分析；
+     * 首先对数组进行排序，排序后固定一个数 nums[i]，再使用左右指针指向 nums[i]后面的两端，
+     * 数字分别为 nums[L]和 nums[R]，计算三个数的和 sum判断是否满足为 0，满足则添加进结果集
+     * 如果 nums[i]大于 0，则三数之和必然无法等于 0，结束循环
+     * 如果 nums[i] == nums[i-1]，则说明该数字重复，会导致结果重复，所以应该跳过
+     * 当 sum == 0 时，nums[L] == nums[L+1]则会导致结果重复，应该跳过，L++
+     * 当 sum == 0 时，nums[R] == nums[R-1] 则会导致结果重复，应该跳过，R--
+     * L == R 则i++
+     * @return: java.util.List<java.util.List < java.lang.Integer>>
+     */
     public static List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> ans = new ArrayList();
         int len = nums.length;

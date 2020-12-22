caa00ce1421fa717c2ec53a4c781068d5db9a6e8
liurui <2258076697@qq.com>
2020-12-22 19:13:58 +0800

    验证二叉搜索树

--- src/LeetCode/Code_98.java
+++ src/LeetCode/Code_98.java
@@ -1,35 +1,44 @@
+package LeetCode;
+
 /**
  * @ClassName: Test72
  * @Description: 验证二叉搜索树
- *给定一个二叉树，判断其是否是一个有效的二叉搜索树。
- *
+ * 给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+ * <p>
  * 假设一个二叉搜索树具有如下特征：
- *
+ * <p>
  * 节点的左子树只包含小于当前节点的数。
  * 节点的右子树只包含大于当前节点的数。
  * 所有左子树和右子树自身必须也是二叉搜索树。
  * 示例 1:
- *
+ * <p>
  * 输入:
- *     2
- *    / \
- *   1   3
+ * 2
+ * / \
+ * 1   3
  * 输出: true
  * 示例 2:
- *
+ * <p>
  * 输入:
- *     5
- *    / \
- *   1   4
- *      / \
- *     3   6
+ * 5
+ * / \
+ * 1   4
+ * / \
+ * 3   6
  * 输出: false
  * 解释: 输入为: [5,1,4,null,null,3,6]。
- *      根节点的值为 5 ，但是其右子节点值为 4 。
+ * 根节点的值为 5 ，但是其右子节点值为 4 。
  * @Author: Admin
  **/
 
-public class Test72 {
+public class Code_98 {
+    /**
+     * @Author: Admin
+     * @Description: 思路描述：
+     * 我们需要一个辅助的指针来进行遍历，整体思路就是比较当前节点的值和 根节点的值然后判断
+     * @param null
+     * @return: null
+     */
     private Integer tempVal = null;
 
     public boolean isValidBST(TreeNode root) {
@@ -54,13 +63,14 @@ public class Test72 {
     }
 
     Integer preValue = null;
+
     public boolean isValidBST1(TreeNode root) {
-        if(root == null){
+        if (root == null) {
             return true;
         }
         //如果左子树是二叉搜索树再继续搜索右子树，如果不是则直接返回
-        if(isValidBST(root.left)){
-            if(preValue != null && preValue >= root.val){
+        if (isValidBST(root.left)) {
+            if (preValue != null && preValue >= root.val) {
                 return false;
             }
             preValue = root.val;

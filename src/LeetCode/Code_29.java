e4a39a80ceaf5497a526658ec0e2e5a7f6c9d20e
liurui <2258076697@qq.com>
2020-12-22 17:35:59 +0800

    两数相除

--- src/LeetCode/Code_29.java
+++ src/LeetCode/Code_29.java
@@ -1,27 +1,40 @@
+package LeetCode;
+
 /**
  * @ClassName: Test57
  * @Description: 两数相除
  * 给定两个整数，被除数 dividend 和除数 divisor。将两数相除，要求不使用乘法、除法和 mod 运算符。
- *
+ * <p>
  * 返回被除数 dividend 除以除数 divisor 得到的商。
- *
- * 整数除法的结果应当截去（truncate）其小数部分，例如：truncate(8.345) = 8 以及 truncate(-2.7335) = -2
- *
+ * <p>
+ * 整数除法的结果应当截去（truncate）其小数部分，例如：truncate(8.345) = 8
+ * 以及 truncate(-2.7335) = -2
+ * <p>
  * 示例 1:
- *
+ * <p>
  * 输入: dividend = 10, divisor = 3
  * 输出: 3
  * 解释: 10/3 = truncate(3.33333..) = truncate(3) = 3
  * 示例 2:
- *
+ * <p>
  * 输入: dividend = 7, divisor = -3
  * 输出: -2
  * 解释: 7/-3 = truncate(-2.33333..) = -2
- *
  * @Author: Admin
  **/
 
-public class Test57 {
+public class Code_29 {
+    /**
+     * @param dividend
+     * @param divisor
+     * @Author: Admin
+     * @Description: 思路描述：
+     * 将两个数转换为负数运算（防止正数发生越界等原因），
+     * 每次都是使用位的左移来判断当前数的的2倍是不是小于被除数，带有符号的左移，符号位不会进行忽略
+     * 用于减少减的次数，然后被除数减去 2倍除数 剩余的继续循环，
+     * 直到被除数大于除数结束循环，最后将结果的正负利用异或进行判断。
+     * @return: int
+     */
     public int divide(int dividend, int divisor) {
         //判断结果的正负
         // 利用对应的 异或运算

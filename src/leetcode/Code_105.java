1b13c3347928483e06e53b7622573360c54437b9
liurui <2258076697@qq.com>
2021-01-08 19:06:07 +0800

    从前序与中序遍历序列构造二叉树

--- src/leetcode/Code_105.java
+++ src/leetcode/Code_105.java
@@ -1,10 +1,12 @@
 package leetcode;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @ClassName: Test74
- * @Description: 从前序与中序遍历构造二叉树
+ * @Description: 从前序与中序遍历序列构造二叉树
  * * 根据一棵树的前序遍历与中序遍历构造二叉树。
  * *
  * * 注意:
@@ -55,4 +57,73 @@ public class Code_105 {
         }
         return root;
     }
+
+    /**
+     * @param preorder
+     * @param inorder
+     * @Author: Admin
+     * @Description: 我们并不需要将对应的 前中序数组分开，我们可以使用指针的缩减，来达到相同的目的。
+     * @return: leetcode.TreeNode
+     */
+    public TreeNode buildTree1(int[] preorder, int[] inorder) {
+        return buildTreeHelper(preorder, 0, preorder.length, inorder, 0, inorder.length);
+    }
+
+    private TreeNode buildTreeHelper(int[] preorder, int pre_start, int pre_end, int[] inorder, int in_start, int in_end) {
+        // 如果前序遍历的结果为空 直接返回。
+        if (pre_start == pre_end) {
+            return null;
+        }
+        // 根据前序遍历的第一个节点作为根节点
+        int rootVal = preorder[pre_start];
+        TreeNode root = new TreeNode(rootVal);
+        // 在中序遍历中的根节点的坐标
+        int index = 0;
+        for (int i = in_start; i < in_end; i++) {
+            if (rootVal == inorder[i]) {
+                index = i;
+                break;
+            }
+        }
+        // 根节点的左子树有多少个子节点
+        int left = index - in_start;
+        root.left = buildTreeHelper(preorder, pre_start + 1, pre_start + left + 1, inorder, in_start, index);
+        root.right = buildTreeHelper(preorder, pre_start + left + 1, pre_end, inorder, index + 1, in_end);
+        return root;
+    }
+
+    /**
+     * @param preorder
+     * @param inorder
+     * @Author: Admin
+     * @Description: 对上一一种算法的优化，使用对应的map保存了中序数组中的下标，省的去遍历查找根节点了。
+     * @return: leetcode.TreeNode
+     */
+    public TreeNode buildTree2(int[] preorder, int[] inorder) {
+        Map<Integer, Integer> map = new HashMap<>();
+        // 将对应的中序数组中的下标加入到hash表中
+        for (int i = 0; i < preorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+        return buildTreeHelper1(preorder, 0, preorder.length, inorder, 0, inorder.length, map);
+    }
+
+    private TreeNode buildTreeHelper1(int[] preorder, int pre_start, int pre_end, int[] inorder, int in_start, int in_end,
+                                      Map<Integer, Integer> map) {
+        // 如果前序遍历的结果为空 直接返回。
+        if (pre_start == pre_end) {
+            return null;
+        }
+        // 根据前序遍历的第一个节点作为根节点
+        int rootVal = preorder[pre_start];
+        TreeNode root = new TreeNode(rootVal);
+        // 在中序遍历中的根节点的坐标
+        int index = map.get(rootVal);
+        // 根节点的左子树有多少个子节点
+        int left = index - in_start;
+        root.left = buildTreeHelper1(preorder, pre_start + 1, pre_start + left + 1, inorder, in_start, index, map);
+        root.right = buildTreeHelper1(preorder, pre_start + left + 1, pre_end, inorder, index + 1, in_end, map);
+        return root;
+    }
+
 }

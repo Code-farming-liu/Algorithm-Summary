c68563fef1377340a178be8030a6432e32425240
刘瑞 <2258076697@qq.com>
2023-01-30 23:24:18 +0800

    力扣 202 快乐数

---
+++ src/leetcode/Code_202.java
@@ -0,0 +1,58 @@
+package leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 快乐数
+ *
+ * 编写一个算法来判断一个数 n 是不是快乐数。
+ *
+ * 「快乐数」 定义为：
+ *
+ * 对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和。
+ * 然后重复这个过程直到这个数变为 1，也可能是 无限循环 但始终变不到 1。
+ * 如果这个过程 结果为 1，那么这个数就是快乐数。
+ * 如果 n 是 快乐数 就返回 true ；不是，则返回 false 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：n = 19
+ * 输出：true
+ * 解释：
+ * 12 + 92 = 82
+ * 82 + 22 = 68
+ * 62 + 82 = 100
+ * 12 + 02 + 02 = 1
+ * 示例 2：
+ *
+ * 输入：n = 2
+ * 输出：false
+ *
+ *
+ * 提示：
+ *
+ * 1 <= n <= 231 - 1
+ */
+public class Code_202 {
+    public boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+        while (n != 1 && !set.contains(n)) {
+            set.add(n);
+            n = getNextNumber(n);
+        }
+        return n == 1;
+    }
+
+    public int getNextNumber(int n) {
+        int res = 0;
+        while (n > 0) {
+            int temp = n % 10;
+            res += temp * temp;
+            n = n / 10;
+        }
+        return res;
+    }
+}

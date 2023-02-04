bff7d8d8a037840a22205e8e754bf3f988819739
刘瑞 <2258076697@qq.com>
2023-02-04 23:40:26 +0800

    lc 383 赎金信

---
+++ src/leetcode/Code_383.java
@@ -0,0 +1,54 @@
+package leetcode;
+
+/**
+ * 给你两个字符串：ransomNote 和 magazine ，判断 ransomNote 能不能由 magazine 里面的字符构成。
+ *
+ * 如果可以，返回 true ；否则返回 false 。
+ *
+ * magazine 中的每个字符只能在 ransomNote 中使用一次。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：ransomNote = "a", magazine = "b"
+ * 输出：false
+ * 示例 2：
+ *
+ * 输入：ransomNote = "aa", magazine = "ab"
+ * 输出：false
+ * 示例 3：
+ *
+ * 输入：ransomNote = "aa", magazine = "aab"
+ * 输出：true
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= ransomNote.length, magazine.length <= 105
+ * ransomNote 和 magazine 由小写英文字母组成
+ *
+ */
+public class Code_383 {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] count = new int[26];
+        if (ransomNote.length() > magazine.length()) {
+            return false;
+        }
+        for (int i = 0; i < magazine.length(); i++) {
+            count[magazine.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < ransomNote.length(); i++) {
+            count[ransomNote.charAt(i) - 'a']--;
+            if (count[ransomNote.charAt(i) - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        new Code_383().canConstruct("aa", "ab");
+    }
+}

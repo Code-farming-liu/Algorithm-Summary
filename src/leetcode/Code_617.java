2c017d6fcd87540d9acda1e5b7fb3beca8d43666
刘瑞 <2258076697@qq.com>
2023-02-18 23:16:49 +0800

    Code_617 合并二叉树

--- src/leetcode/Code_617.java
+++ src/leetcode/Code_617.java
@@ -29,7 +29,7 @@ package leetcode;
  * -104 <= Node.val <= 104
  */
 public class Code_617 {
-    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+    public TreeNode mergeTrees1(TreeNode root1, TreeNode root2) {
         if (root1 == null) {
             return root2;
         }
@@ -37,8 +37,21 @@ public class Code_617 {
             return root1;
         }
         TreeNode node = new TreeNode(root1.val + root2.val);
-        node.left = mergeTrees(root1.left, root2.left);
-        node.right = mergeTrees(root1.right, root2.right);
+        node.left = mergeTrees1(root1.left, root2.left);
+        node.right = mergeTrees1(root1.right, root2.right);
         return node;
+    }
+
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null) {
+            return root2;
+        }
+        if (root2 == null) {
+            return root1;
+        }
+        root1.val += root2.val;
+        root1.left = mergeTrees(root1.left, root2.left);
+        root1.right = mergeTrees(root1.right, root2.right);
+        return root1;
     }
 }

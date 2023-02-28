3183904b7a91568e0754ee9f1a161f177cfc3f98
刘瑞 <2258076697@qq.com>
2023-02-28 23:54:52 +0800

    code 235 二叉搜索树的最近公共祖先

--- src/leetcode/Code_235.java
+++ src/leetcode/Code_235.java
@@ -55,4 +55,17 @@ public class Code_235 {
         }
         return root;
     }
+
+    public TreeNode lowestCommonAncestor1(TreeNode root, TreeNode p, TreeNode q) {
+        while (root != null) {
+            if (root.val > p.val && root.val > q.val) {
+                root = root.left;
+            } else if (root.val < p.val && root.val < q.val) {
+                root = root.right;
+            } else {
+                return root;
+            }
+        }
+        return null;
+    }
 }

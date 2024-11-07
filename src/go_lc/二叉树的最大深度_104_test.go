573f315aa98c911bdc9dc72919cce76ef396567a
liurui <2258076697@qq.com>
2024-11-07 23:21:49 +0800

    add 二叉树的最大/最小深度 -go

---
+++ src/go_lc/二叉树的最大深度_104_test.go
@@ -0,0 +1,37 @@
+package leetcode
+
+/**
+给定一个二叉树 root ，返回其最大深度。
+
+二叉树的 最大深度 是指从根节点到最远叶子节点的最长路径上的节点数。
+
+
+
+示例 1：
+
+
+
+
+
+输入：root = [3,9,20,null,null,15,7]
+输出：3
+示例 2：
+
+输入：root = [1,null,2]
+输出：2
+
+
+提示：
+
+树中节点的数量在 [0, 104] 区间内。
+-100 <= Node.val <= 100
+*/
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	left := maxDepth(root.Left)
+	right := maxDepth(root.Right)
+	return 1 + max(left, right)
+}

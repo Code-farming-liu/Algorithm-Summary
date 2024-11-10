bd15d9eb113d8f5284d95c1f37e32a31b4c55d4b
liurui <2258076697@qq.com>
2024-11-11 00:07:43 +0800

    add 437 -go

---
+++ src/go_lc/路径总和III_437_test.go
@@ -0,0 +1,69 @@
+package leetcode
+
+/*
+*
+给定一个二叉树的根节点 root ，和一个整数 targetSum ，求该二叉树里节点值之和等于 targetSum 的 路径 的数目。
+
+路径 不需要从根节点开始，也不需要在叶子节点结束，但是路径方向必须是向下的（只能从父节点到子节点）。
+
+示例 1：
+
+输入：root = [10,5,-3,3,2,null,11,3,-2,null,1], targetSum = 8
+输出：3
+解释：和等于 8 的路径有 3 条，如图所示。
+示例 2：
+
+输入：root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+输出：3
+
+提示:
+
+二叉树的节点个数的范围是 [0,1000]
+-109 <= Node.val <= 109
+-1000 <= targetSum <= 1000
+*/
+func sum(root *TreeNode, targetSum int) int {
+	res := 0
+	if root == nil {
+		return 0
+	}
+	var dfs func(*TreeNode, int)
+	dfs = func(node *TreeNode, target int) {
+		if node == nil {
+			return
+		}
+		target -= node.Val
+		if target == 0 {
+			res++
+		}
+		dfs(node.Left, target)
+		dfs(node.Right, target)
+	}
+	dfs(root, targetSum)
+	return res
+}
+
+func pathSum3(root *TreeNode, targetSum int) int {
+	if root == nil {
+		return 0
+	}
+
+	stack := []*TreeNode{}
+	c := []*TreeNode{}
+	for root != nil || len(stack) > 0 {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+		root = stack[len(stack)-1]
+		c = append(c, root)
+		stack = stack[:len(stack)-1]
+		root = root.Right
+	}
+
+	res := 0
+	for _, v := range c {
+		res += sum(v, targetSum)
+	}
+	return res
+}

5cb6d7b3feadbdbb2da359260c86d79eba25d53b
liurui <2258076697@qq.com>
2024-11-17 17:03:12 +0800

    add code1480-java

---
+++ src/leetcode/code1480.java
@@ -0,0 +1,41 @@
+package leetcode;
+
+/**
+ * 给你一个数组 nums 。数组「动态和」的计算公式为：runningSum[i] = sum(nums[0]…nums[i]) 。
+ * <p>
+ * 请返回 nums 的动态和。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [1,2,3,4]
+ * 输出：[1,3,6,10]
+ * 解释：动态和计算过程为 [1, 1+2, 1+2+3, 1+2+3+4] 。
+ * 示例 2：
+ * <p>
+ * 输入：nums = [1,1,1,1,1]
+ * 输出：[1,2,3,4,5]
+ * 解释：动态和计算过程为 [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1] 。
+ * 示例 3：
+ * <p>
+ * 输入：nums = [3,1,2,10,1]
+ * 输出：[3,4,6,16,17]
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 1000
+ * -10^6 <= nums[i] <= 10^6
+ */
+public class code1480 {
+    public int[] runningSum(int[] nums) {
+        int[] res = new int[nums.length];
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            res[i] = sum;
+        }
+        return res;
+    }
+}

5f7e999327a60d3f34b957fe50a476418f395c0b
liurui <2258076697@qq.com>
2024-11-17 16:54:34 +0800

    add  1480-go

---
+++ src/go_lc/寻找数组的中心下标_724_test.go
@@ -0,0 +1,32 @@
+package leetcode
+
+func pivotIndex1(nums []int) int {
+	left, right := 0, 0
+	for i := 0; i < len(nums); i++ {
+		right += nums[i]
+	}
+
+	for i := 0; i < len(nums); i++ {
+		left += nums[i]
+		if right == left {
+			return i
+		}
+		right -= nums[i]
+	}
+	return -1
+}
+
+func pivotIndex(nums []int) int {
+	left, total := 0, 0
+	for i := 0; i < len(nums); i++ {
+		total += nums[i]
+	}
+
+	for i := 0; i < len(nums); i++ {
+		if total == 2*left+nums[i] {
+			return i
+		}
+		left += nums[i]
+	}
+	return -1
+}

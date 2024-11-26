0614ffe92a1f82adf387ee432b5a01607f0248b1
liurui <2258076697@qq.com>
2024-11-26 21:29:15 +0800

    add 958

---
+++ src/leetcode/code_958.java
@@ -0,0 +1,60 @@
+package leetcode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 给你一棵二叉树的根节点 root ，请你判断这棵树是否是一棵 完全二叉树 。
+ *
+ * 在一棵 完全二叉树 中，除了最后一层外，所有层都被完全填满，并且最后一层中的所有节点都尽可能靠左。最后一层（第 h 层）中可以包含 1 到 2h 个节点。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ *
+ * 输入：root = [1,2,3,4,5,6]
+ * 输出：true
+ * 解释：最后一层前的每一层都是满的（即，节点值为 {1} 和 {2,3} 的两层），且最后一层中的所有节点（{4,5,6}）尽可能靠左。
+ * 示例 2：
+ *
+ *
+ *
+ * 输入：root = [1,2,3,4,5,null,7]
+ * 输出：false
+ * 解释：值为 7 的节点不满足条件「节点尽可能靠左」。
+ *
+ *
+ * 提示：
+ *
+ * 树中节点数目在范围 [1, 100] 内
+ * 1 <= Node.val <= 1000
+ */
+public class code_958 {
+    public boolean isCompleteTree(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        boolean empty = false;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node == null) {
+                    empty = true;
+                    continue;
+                }
+                if (empty) {
+                    return false;
+                }
+                queue.add(node.left);
+                queue.add(node.right);
+            }
+        }
+        return true;
+    }
+}

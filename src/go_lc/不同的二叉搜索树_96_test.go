99b3d8489044c266acbe9bd996c05afa4ba31bba
liurui <2258076697@qq.com>
2024-12-01 19:22:59 +0800

    add 96-go

---
+++ src/go_lc/不同的二叉搜索树_96_test.go
@@ -0,0 +1,29 @@
+package leetcode
+
+/**
+给你一个整数 n ，求恰由 n 个节点组成且节点值从 1 到 n 互不相同的 二叉搜索树 有多少种？返回满足题意的二叉搜索树的种数。
+
+
+
+示例 1：
+
+
+输入：n = 3
+输出：5
+示例 2：
+
+输入：n = 1
+输出：1
+*/
+
+func numTrees(n int) int {
+	dp := make([]int, n+1)
+	dp[0] = 1
+	dp[1] = 1
+	for i := 2; i < n; i++ {
+		for j := 1; j < i; j++ {
+			dp[i] += dp[j-1] * dp[i-j]
+		}
+	}
+	return dp[n]
+}

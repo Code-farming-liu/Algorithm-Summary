97393afae072fcdc1594a6b7ae239eb43539f2eb
liurui <2258076697@qq.com>
2025-01-01 21:33:11 +0800

    add  21-go

---
+++ src/go_lc/listnode/合并两个有序链表_21_test.go
@@ -0,0 +1,55 @@
+package listnode
+
+/*
+*
+将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+
+示例 1：
+
+输入：l1 = [1,2,4], l2 = [1,3,4]
+输出：[1,1,2,3,4,4]
+示例 2：
+
+输入：l1 = [], l2 = []
+输出：[]
+示例 3：
+
+输入：l1 = [], l2 = [0]
+输出：[0]
+
+提示：
+
+两个链表的节点数目范围是 [0, 50]
+-100 <= Node.val <= 100
+l1 和 l2 均按 非递减顺序 排列
+*/
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	if list1 == nil {
+		return list2
+	}
+	if list2 == nil {
+		return list1
+	}
+
+	dummyHead := &ListNode{Val: 0}
+	curr := dummyHead
+	p1, p2 := list1, list2
+	for p1 != nil && p2 != nil {
+		if p1.Val < p2.Val {
+			curr.Next = p1
+			p1 = p1.Next
+		} else {
+			curr.Next = p2
+			p2 = p2.Next
+		}
+		curr = curr.Next
+	}
+
+	if p1 != nil {
+		curr.Next = p1
+	}
+	if p2 != nil {
+		curr.Next = p2
+	}
+	return dummyHead.Next
+}

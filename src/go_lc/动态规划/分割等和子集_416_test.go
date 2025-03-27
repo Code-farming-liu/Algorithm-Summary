03b72257aeb7c5741b3fb3619688de9b2a538c10
liurui <2258076697@qq.com>
2025-03-27 23:10:55 +0800

    add 416

---
+++ src/go_lc/动态规划/分割等和子集_416_test.go
@@ -0,0 +1,44 @@
+package 动态规划
+
+/**
+给你一个 只包含正整数 的 非空 数组 nums 。请你判断是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。
+
+
+
+示例 1：
+
+输入：nums = [1,5,11,5]
+输出：true
+解释：数组可以分割成 [1, 5, 5] 和 [11] 。
+示例 2：
+
+输入：nums = [1,2,3,5]
+输出：false
+解释：数组不能分割成两个元素和相等的子集。
+
+
+提示：
+
+1 <= nums.length <= 200
+1 <= nums[i] <= 100
+*/
+
+func canPartition(nums []int) bool {
+	sum := 0
+	for _, num := range nums {
+		sum += num
+	}
+
+	if sum%2 != 0 {
+		return false
+	}
+
+	target := sum / 2
+	dp := make([]int, target+1)
+	for i := 0; i < len(nums); i++ {
+		for j := target; j >= nums[i]; j-- {
+			dp[j] = max(dp[j], dp[j-nums[i]]+nums[i])
+		}
+	}
+	return dp[target] == target
+}

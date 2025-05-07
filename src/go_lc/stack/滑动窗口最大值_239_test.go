89ba3acd9aedb4ab063619e83a234b5997837fa6
liurui <2258076697@qq.com>
2025-05-07 23:46:09 +0800

    add ...

---
+++ src/go_lc/stack/滑动窗口最大值_239_test.go
@@ -0,0 +1,91 @@
+package stack
+
+/*
+*
+给你一个整数数组 nums，有一个大小为 k 的滑动窗口从数组的最左侧移动到数组的最右侧。你只可以看到在滑动窗口内的 k 个数字。滑动窗口每次只向右移动一位。
+
+返回 滑动窗口中的最大值 。
+
+示例 1：
+
+输入：nums = [1,3,-1,-3,5,3,6,7], k = 3
+输出：[3,3,5,5,6,7]
+解释：
+滑动窗口的位置                最大值
+---------------               -----
+[1  3  -1] -3  5  3  6  7       3
+
+	1 [3  -1  -3] 5  3  6  7       3
+	1  3 [-1  -3  5] 3  6  7       5
+	1  3  -1 [-3  5  3] 6  7       5
+	1  3  -1  -3 [5  3  6] 7       6
+	1  3  -1  -3  5 [3  6  7]      7
+
+示例 2：
+
+输入：nums = [1], k = 1
+输出：[1]
+
+提示：
+
+1 <= nums.length <= 105
+-104 <= nums[i] <= 104
+1 <= k <= nums.length
+*/
+// 封装单调队列的方式解题
+type MyQueue struct {
+	queue []int
+}
+
+func NewMyQueue() *MyQueue {
+	return &MyQueue{
+		queue: make([]int, 0),
+	}
+}
+
+func (m *MyQueue) Front() int {
+	return m.queue[0]
+}
+
+func (m *MyQueue) Back() int {
+	return m.queue[len(m.queue)-1]
+}
+
+func (m *MyQueue) Empty() bool {
+	return len(m.queue) == 0
+}
+
+func (m *MyQueue) Push(val int) {
+	for !m.Empty() && val > m.Back() {
+		m.queue = m.queue[:len(m.queue)-1]
+	}
+	m.queue = append(m.queue, val)
+}
+
+func (m *MyQueue) Pop(val int) {
+	if !m.Empty() && val == m.Front() {
+		m.queue = m.queue[1:]
+	}
+}
+
+func maxSlidingWindow(nums []int, k int) []int {
+	queue := NewMyQueue()
+	length := len(nums)
+	res := make([]int, 0)
+	// 先将前k个元素放入队列
+	for i := 0; i < k; i++ {
+		queue.Push(nums[i])
+	}
+	// 记录前k个元素的最大值
+	res = append(res, queue.Front())
+
+	for i := k; i < length; i++ {
+		// 滑动窗口移除最前面的元素
+		queue.Pop(nums[i-k])
+		// 滑动窗口添加最后面的元素
+		queue.Push(nums[i])
+		// 记录最大值
+		res = append(res, queue.Front())
+	}
+	return res
+}
